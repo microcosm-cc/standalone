921730b3f9a0d722efa9b2013e399ac5006a888a
Matt Cottingham <matt.cottingham@gmail.com>
2014-09-22 13:54:10 +0100

    Add postgresql database config

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,5 +1,16 @@
+import "secret.pp"
+
 node 'dev.microco.sm' {
 
-    class {'postgresql::server':}
+    class {'postgresql::server':
+        ip_mask_allow_all_users    => '0.0.0.0/0',
+        listen_addresses           => '*',
+        ipv4acls                   => ['host microcosm microcosm * md5'],
+    }
+
+    postgresql::server::db { 'microcosm':
+        user     => 'microcosm',
+        password => $microcosm_pg_pass,
+    }
 
 }
